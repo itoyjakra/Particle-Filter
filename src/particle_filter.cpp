db2fb3d4e151ffb6fc69ba9c910d2d69d5dbbcaa
--local <--local>
2017-06-07 21:03:59 -0500

    Updated weight calculation and resampling

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -20,7 +20,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[])
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
-    num_particles = 100;
+    num_particles = 25;
     double std_x = std[0];
     double std_y = std[1];
     double std_theta = std[2];
@@ -82,7 +82,7 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
     std::cout << "observations size = " << observations.size() << std::endl;
 }
 
-void transform_coord_list(Particle p, std::vector<LandmarkObs> &obs)
+std::vector<LandmarkObs> transform_coord_list(Particle p, std::vector<LandmarkObs> obs)
 {
     double x = p.x;
     double y = p.y;
@@ -91,7 +91,7 @@ void transform_coord_list(Particle p, std::vector<LandmarkObs> &obs)
         obs[i].x = p.x + obs[i].x * cos(p.theta) - obs[i].y * sin(p.theta);
         obs[i].y = p.y + obs[i].x * sin(p.theta) + obs[i].y * cos(p.theta);
     }
-
+    return obs;
 }
 
 void transform_coord(Particle p, LandmarkObs &obs)
@@ -130,19 +130,23 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
         std::vector<double> landmark_x_list;
         std::vector<double> landmark_y_list;
 
-        for (int j=0; j<observations.size(); j++)
+        std::cout << "trans_coord for part: " << i << ":: " << p.x << ", " << p.y << std::endl;
+        std::vector<LandmarkObs> obs = transform_coord_list(p, observations);
+        for (int j=0; j<obs.size(); j++)
         {
-            LandmarkObs obs = observations[j];
-            transform_coord(p, obs);
+            //LandmarkObs obs = observations[j];
+            //std::cout << "obs before: " << j << ":: " << obs.x << ", " << obs.y << std::endl;
+            //transform_coord(p, obs);
+            //std::cout << "obs after: " << j << ":: " << obs.x << ", " << obs.y << std::endl;
 
-            double min_dist = 0;
+            double min_dist = 1.0e5;
             int closest_landmark;
-
-
+            double x1 = obs[j].x;
+            double y1 = obs[j].y;
             for (int l=0; l<map_landmarks.landmark_list.size(); l++)
             {
-                double x1 = p.x;
-                double y1 = p.y;
+                //double x1 = p.x;
+                //double y1 = p.y;
                 double x2 = map_landmarks.landmark_list[l].x_f;
                 double y2 = map_landmarks.landmark_list[l].y_f;
                 double d = dist(x1, y1, x2, y2);
@@ -152,6 +156,7 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
                     closest_landmark = l;
                 }
             }
+            std::cout << i << " " << j << "-- min dist = " << min_dist << " closest lm id = " << closest_landmark << std::endl;
             landmark_id_list.push_back(map_landmarks.landmark_list[closest_landmark].id_i);
             landmark_x_list.push_back(map_landmarks.landmark_list[closest_landmark].x_f);
             landmark_y_list.push_back(map_landmarks.landmark_list[closest_landmark].y_f);
@@ -160,13 +165,16 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
         assert (observations.size() == p.associations.size());
 
         double sum = 0;
-        for (int j=0; j<observations.size(); j++)
+        for (int j=0; j<obs.size(); j++)
         {
-            LandmarkObs obs = observations[j];
-            sum += pow(p.sense_x[j] - obs.x, 2) / (2 * sigma_x * sigma_x);
-            sum += pow(p.sense_y[j] - obs.y, 2) / (2 * sigma_y * sigma_y);
+            LandmarkObs iobs = obs[j];
+            sum += pow(p.sense_x[j] - iobs.x, 2) / (2 * sigma_x * sigma_x);
+            sum += pow(p.sense_y[j] - iobs.y, 2) / (2 * sigma_y * sigma_y);
+            //std::cout << j << ": " << p.sense_x[j] << ", " << p.sense_y[j] << "| " << iobs.x << ", " << iobs.y << std::endl;
         }
-        p.weight = exp(-sum) / pow(2 * sigma_x * sigma_y, observations.size());
+        p.weight = exp(-sum) / pow(2 * sigma_x * sigma_y, obs.size());
+        particles[i].weight = p.weight;
+        std::cout << "^^^ " << i << ", " << sum << ", " << p.weight << std::endl;
     }
 
 }
@@ -179,6 +187,15 @@ void ParticleFilter::resample()
 
     std::vector<double> w_dist;
     std::vector<int> id_dist;
+
+    double max_weight = 0;
+    for (int i=0; i<num_particles; i++)
+    {
+        std::cout << "weight for particle " << i << " = " << particles[i].weight << std::endl;
+        if (particles[i].weight > max_weight)
+            max_weight = particles[i].weight;
+    }
+    std::cout << "in resample, max weight = " << max_weight << std::endl;
 
     for (int i=0; i<num_particles; i++)
     {
@@ -196,13 +213,30 @@ void ParticleFilter::resample()
 
     std::vector<Particle> refresh_p;
     for (int i=0; i<num_particles; i++)
+    {
+        std::cout << "--- " << i << ", " << m[i] << std::endl;
+        int c = m[i];
+        while (c > 0)
+        {
+            refresh_p.push_back(particles[i]);
+            c--;
+        }
+    }
+    std::cout << "new number of particles = " << refresh_p.size() << std::endl;
+    std::cout << "old number of particles = " << particles.size() << std::endl;
+    assert (refresh_p.size() == particles.size());
+    particles = refresh_p;
+
+
+    /*
+    std::vector<Particle> refresh_p;
+    for (int i=0; i<num_particles; i++)
         if (m[i] > 0)
             refresh_p.push_back(particles[i]);
     num_particles = refresh_p.size();
     particles = refresh_p;
     std::cout << "new number of particles = " << num_particles << std::endl;
 
-    /*
     std::vector<int> erase_list;
     for (int i=0; i<num_particles; i++)
     {
